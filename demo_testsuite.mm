bb448cfc4237d269d5c30abe2dda2be2f2079347
lanyufeng <lanyufeng@aspirecn.com>
2022-06-01 02:00:45 +0800

    更正样例，已带编号的步骤的预期行为也必须有编号。

--- demo_testsuite.mm
+++ demo_testsuite.mm
@@ -1,7 +1,8 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1652002985538" ID="ID_1423958250" MODIFIED="1652583092939" TEXT="root node">
-<node CREATED="1652003000558" ID="ID_836860179" MODIFIED="1652960390668" POSITION="right" TEXT="&quot;lvl1&quot; module &#x6a21;&#x5757;1">
+<node CREATED="1652002985538" ID="ID_1423958250" MODIFIED="1654005271388" TEXT="root node">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
+<node CREATED="1652003000558" ID="ID_836860179" MODIFIED="1654005271384" POSITION="right" TEXT="&quot;lvl1&quot; module &#x6a21;&#x5757;1">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -23,10 +24,12 @@
     </ol>
   </body>
 </html></richcontent>
-<node CREATED="1652003022725" ID="ID_752501720" MODIFIED="1652003506729" TEXT="case 1-1_P0">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
+<node CREATED="1652003022725" ID="ID_752501720" MODIFIED="1654005271384" TEXT="case 1-1_P0">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
 <icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1652003040001" ID="ID_945780629" MODIFIED="1652003867741" TEXT="case 1-2_P2">
+<node CREATED="1652003040001" ID="ID_945780629" MODIFIED="1654005271384" TEXT="case 1-2_P2">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -37,12 +40,14 @@
     </p>
   </body>
 </html></richcontent>
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
 <icon BUILTIN="closed"/>
 <icon BUILTIN="button_cancel"/>
-<node CREATED="1652003060121" ID="ID_715471655" MODIFIED="1652003517230" TEXT="case 1-2_step1">
+<node CREATED="1652003060121" ID="ID_715471655" MODIFIED="1654005271384" TEXT="case 1-2_step1">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
 <icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1652003071792" ID="ID_426226989" MODIFIED="1652003859937" TEXT="case 1-2_step2 &amp; expect2">
+<node CREATED="1652003071792" ID="ID_426226989" MODIFIED="1654005271384" TEXT="case 1-2_step2 &amp; expect2">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -53,19 +58,30 @@
     </p>
   </body>
 </html></richcontent>
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
 <icon BUILTIN="button_cancel"/>
 </node>
 </node>
-<node CREATED="1652003045217" ID="ID_996949815" MODIFIED="1652003054866" TEXT="other_lvl2_node"/>
+<node CREATED="1652003045217" ID="ID_996949815" MODIFIED="1654005271384" TEXT="other_lvl2_node">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
 </node>
-<node CREATED="1652003009370" ID="ID_125859384" MODIFIED="1652957284096" POSITION="right" TEXT="lvl1 module2">
-<node CREATED="1652003105463" ID="ID_1636515565" MODIFIED="1652957917899" TEXT="&#x7528;&#x4f8b;case2-1_P1">
+</node>
+<node CREATED="1652003009370" ID="ID_125859384" MODIFIED="1654005271382" POSITION="right" TEXT="lvl1 module2">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
+<node CREATED="1652003105463" ID="ID_1636515565" MODIFIED="1654005271381" TEXT="&#x7528;&#x4f8b;case2-1_P1">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
 <icon BUILTIN="stop-sign"/>
-<node CREATED="1652583133344" ID="ID_427831947" MODIFIED="1652957973315" TEXT="1. case2-1 &#x6b65;&#x9aa4;step1 &amp; 1. expect1"/>
-<node CREATED="1652583139530" ID="ID_192290723" MODIFIED="1652583233026" TEXT="2. case2-1 step2"/>
-<node CREATED="1652583164732" ID="ID_296579251" MODIFIED="1652583229814" TEXT="3. case2-1 step3 &amp; 3. expect3"/>
+<node CREATED="1652583133344" ID="ID_427831947" MODIFIED="1654005271378" TEXT="1. case2-1 &#x6b65;&#x9aa4;step1 &amp; 1. expect1">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
 </node>
-<node CREATED="1652003248812" ID="ID_960825595" MODIFIED="1652960396861">
+<node CREATED="1652583139530" ID="ID_192290723" MODIFIED="1654019955238" TEXT="2. case2-1 step2 &amp; 2. ">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
+</node>
+<node CREATED="1652583164732" ID="ID_296579251" MODIFIED="1654005271377" TEXT="3. case2-1 step3 &amp; 3. expect3">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
+</node>
+</node>
+<node CREATED="1652003248812" ID="ID_960825595" MODIFIED="1654005271370">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -82,36 +98,48 @@
     </p>
   </body>
 </html></richcontent>
-<node CREATED="1652003120081" ID="ID_1885981933" MODIFIED="1652989206236" TEXT="case2-2-1 titile_P0">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
+<node CREATED="1652003120081" ID="ID_1885981933" MODIFIED="1654005271369" TEXT="case2-2-1 titile_P0">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
 <icon BUILTIN="button_ok"/>
-<node CREATED="1652003140419" ID="ID_1892482965" MODIFIED="1652003570958" TEXT="case2-2 step 1 &amp; expect1">
-<icon BUILTIN="button_ok"/>
-</node>
-<node CREATED="1652003140419" ID="ID_1541744537" MODIFIED="1652989223174" TEXT="case2-2 step 2 &amp; expect2">
-<icon BUILTIN="button_ok"/>
-</node>
-<node CREATED="1652003164491" ID="ID_330217734" MODIFIED="1652003570958" TEXT="case2-2 step3 &amp; expect 3">
+<node CREATED="1652003140419" ID="ID_1892482965" MODIFIED="1654005271369" TEXT="case2-2 step 1 &amp; expect1">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
 <icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1652003362905" ID="ID_1706704108" MODIFIED="1652003391995" TEXT="case 2-2 step4">
-<node CREATED="1652003394038" ID="ID_630618332" MODIFIED="1652003579854" TEXT="step4-1">
+<node CREATED="1652003140419" ID="ID_1541744537" MODIFIED="1654005271368" TEXT="case2-2 step 2 &amp; expect2">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
 <icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1652003408450" ID="ID_352881205" MODIFIED="1652003579858" TEXT="step 4-2">
+<node CREATED="1652003164491" ID="ID_330217734" MODIFIED="1654005271368" TEXT="case2-2 step3 &amp; expect 3">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
 <icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1652003412452" ID="ID_722085504" MODIFIED="1652003579856" TEXT="step 4-3 &amp; expect 4-3">
+<node CREATED="1652003362905" ID="ID_1706704108" MODIFIED="1654005271368" TEXT="case 2-2 step4">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
+<node CREATED="1652003394038" ID="ID_630618332" MODIFIED="1654005271368" TEXT="step4-1">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
 <icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1652003423673" ID="ID_1517365000" MODIFIED="1652992124062" TEXT="step 4-4">
+<node CREATED="1652003408450" ID="ID_352881205" MODIFIED="1654005271368" TEXT="step 4-2">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1652003412452" ID="ID_722085504" MODIFIED="1654005271367" TEXT="step 4-3 &amp; expect 4-3">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1652003423673" ID="ID_1517365000" MODIFIED="1654005271367" TEXT="step 4-4">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
 <icon BUILTIN="button_ok"/>
 </node>
 </node>
-<node CREATED="1652003884520" ID="ID_1152797934" MODIFIED="1652003889990" TEXT="case 2-2 step5">
-<node CREATED="1652003394038" ID="ID_1286638101" MODIFIED="1652003910217" TEXT="step5-1">
+<node CREATED="1652003884520" ID="ID_1152797934" MODIFIED="1654005271366" TEXT="case 2-2 step5">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
+<node CREATED="1652003394038" ID="ID_1286638101" MODIFIED="1654005271366" TEXT="step5-1">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
 <icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1652003408450" ID="ID_261265745" MODIFIED="1652009970455" TEXT="step 5-2">
+<node CREATED="1652003408450" ID="ID_261265745" MODIFIED="1654005271364" TEXT="step 5-2">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -122,14 +150,16 @@
     </p>
   </body>
 </html></richcontent>
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
 <icon BUILTIN="button_ok"/>
 </node>
 </node>
 </node>
 </node>
 </node>
-<node CREATED="1652003195630" ID="ID_837532163" MODIFIED="1652003201014" POSITION="right" TEXT="other lvl1 node">
-<node CREATED="1652003205947" ID="ID_1453815754" MODIFIED="1652003849935" TEXT="lvl2 node1">
+<node CREATED="1652003195630" ID="ID_837532163" MODIFIED="1654005271363" POSITION="right" TEXT="other lvl1 node">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
+<node CREATED="1652003205947" ID="ID_1453815754" MODIFIED="1654005271362" TEXT="lvl2 node1">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -140,8 +170,11 @@
     </p>
   </body>
 </html></richcontent>
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
 </node>
-<node CREATED="1652003217467" ID="ID_1843749156" MODIFIED="1652003220222" TEXT="lvl2 node2"/>
+<node CREATED="1652003217467" ID="ID_1843749156" MODIFIED="1654005271356" TEXT="lvl2 node2">
+<font NAME="&#x667a;&#x52c7;&#x624b;&#x4e66;&#x4f53;" SIZE="18"/>
+</node>
 </node>
 </node>
 </map>
